28cbb86b91834fbb79a67df55449cdbf6bb85c4b
Caitlin <caitlin.reyes93@myhunter.cuny.edu>
2025-08-13 16:40:55 -0400

    Finished challenge

--- arcade.py
+++ arcade.py
@@ -22,11 +22,26 @@
 
 name = input("What is your name? ")
 passes = int(input("How many passes do you want? "))
-tokens = int(input("How many tokens do you want on your pass(es)? "))
+
+# Let's say this arcade is small, so they only have one tier of pass available to buy. Each pass contains 90 tokens. One pass costs $45.00.
+
+tokens_per_pass = 90
+price_per_pass = 45.00
+
+total_tokens = passes*tokens_per_pass
+total_cost = passes*price_per_pass
+
+# Each game in the arcade costs 7 tokens to play. 
+
+games_available = total_tokens // 7
 
 
 
-print(f'Welcome {name}!')
+
+print(f"\nHere's your receipt summary: \n")
+print(f'Customer Name: {name}')
 print(f'You bought {passes} passes.')
-print(f'You have {tokens} tokens per pass.')
+print(f'You have {total_tokens} tokens total.')
+print(f'Your total cost is: ${total_cost:.2f}')
+print(f'You can play {games_available} games with this purchase.')
 
